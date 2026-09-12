3105b452a17e481cc25f528876d607284b03bc2f
selengr <rezaa997karbakhsh@gmail.com>
2026-09-12 21:41:52 +0330

    switch jest config to js (ts-node was annoying)

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   setupFiles: ['<rootDir>/tests/setup-env.ts'],
   testTimeout: 30000,
 };
-
-export default config;
